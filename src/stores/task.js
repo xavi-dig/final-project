ad6f463a39fb9d1f87ef5be3aa99d9fd550e61a9
xavi-dig <xromero82@gmail.com>
2022-10-03 23:52:32 +0200

    first commit

---
+++ src/stores/task.js
@@ -0,0 +1,45 @@
+import { defineStore } from "pinia";
+import { supabase } from "../supabase";
+import { useUserStore } from "./user";
+
+export const useTaskStore = defineStore("tasks", {
+  state: () => ({
+    tasks: null,
+  }),
+  actions: {
+    async fetchTasks() {
+      const { data: tasks } = await supabase
+        .from("tasks")
+        .select("*")
+        .order("id", { ascending: false });
+      this.tasks = tasks;
+      return this.tasks;
+    },
+    // New code
+    async addTask(title, description) {
+      console.log(useUserStore().user.id);
+      const { data, error } = await supabase.from("tasks").insert([
+        {
+          user_id: useUserStore().user.id,
+          title: title,
+          is_complete: false,
+          description: description,
+        },
+      ]);
+    },
+
+    //New Code 2
+    async completeTask(taskID) {
+      const { data, error } = await supabase
+        .from("tasks")
+        .update({ is_complete: true })
+        .eq("id", taskID);
+    },
+    async deleteAllTasks() {
+      const { data, error } = await supabase
+        .from("tasks")
+        .delete()
+        .eq("is_complete", false);
+    },
+  },
+});
