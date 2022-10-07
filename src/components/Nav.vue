0c750cafb180e2fc325ce03921ea83fd9af19ebe
xavi-dig <xromero82@gmail.com>
2022-10-07 18:08:32 +0200

    Cambio Logo e inicio adaptación Nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -58,16 +58,12 @@
     class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900"
   >
     <div class="container flex flex-wrap justify-between items-center mx-auto">
-      <a href="https://flowbite.com/" class="flex items-center">
+      <a href="/" class="flex items-center">
         <img
-          src="https://flowbite.com/docs/images/logo.svg"
+          src="https://res.cloudinary.com/dnsnkrcru/image/upload/v1648481844/taskApp/imgs/logo-small_bh8xj2.svg"
           class="mr-3 h-6 sm:h-9"
-          alt="Flowbite Logo"
+          alt="TaskApp Logo"
         />
-        <span
-          class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
-          >Flowbite</span
-        >
       </a>
       <button
         data-collapse-toggle="navbar-default"
